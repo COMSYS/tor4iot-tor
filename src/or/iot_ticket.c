b92b37cfe8bfc6e6f4015a2ad83373dcf8482468
Markus Dahlmanns <markus.dahlmanns@rwth-aachen.de>
2018-05-14 14:39:08 +0200

    Disable assert

--- src/or/iot_ticket.c
+++ src/or/iot_ticket.c
@@ -36,7 +36,7 @@ void iot_ticket_send(origin_circuit_t *circ) {
 
   // Set SP address in ticket
   // XXX: THIS IS NOT THE UDP PORT, WILL FAIL IF IT IS DIFFERENT FROM TCP PORT
-  tor_assert(split_point->extend_info->addr.family == AF_INET6);
+  //tor_assert(split_point->extend_info->addr.family == AF_INET6);
   memcpy(&msg->ticket.sp_address.in_addr, &split_point->extend_info->addr.addr.in6_addr, 4*32);
   msg->ticket.sp_address.port = split_point->extend_info->port;
 
@@ -62,6 +62,7 @@ void iot_ticket_send(origin_circuit_t *circ) {
   encrypt = aes_new_cipher(key, iv, 128);
   aes_crypt_inplace(encrypt, (char*) &msg->ticket, sizeof(iot_ticket_t)-DIGEST256_LEN);
   aes_cipher_free(encrypt);
+
 
   //Compute MAC
   crypto_hmac_sha256((char*) msg->ticket.mac, (char*) mac_key, 16, (char*) &msg->ticket, sizeof(iot_ticket_t)-16);
