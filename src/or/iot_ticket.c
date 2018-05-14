2c37b9120eb3691763507d0e77f11929d955c3ec
Markus Dahlmanns <markus.dahlmanns@rwth-aachen.de>
2018-05-14 15:05:21 +0200

    Add C implementation of UDP testserver

--- src/or/iot_ticket.c
+++ src/or/iot_ticket.c
@@ -56,13 +56,10 @@ void iot_ticket_send(origin_circuit_t *circ) {
   memcpy(&msg->ticket.exit_b.aes_key, split_point->next->next->b_aesctrkey, CIPHER_KEY_LEN);
   memcpy(&msg->ticket.exit_f.aes_key, split_point->next->next->f_aesctrkey, CIPHER_KEY_LEN);
 
-
-
   //Encrypt ticket
   encrypt = aes_new_cipher(key, iv, 128);
   aes_crypt_inplace(encrypt, (char*) &msg->ticket, sizeof(iot_ticket_t)-DIGEST256_LEN);
   aes_cipher_free(encrypt);
-
 
   //Compute MAC
   crypto_hmac_sha256((char*) msg->ticket.mac, (char*) mac_key, 16, (char*) &msg->ticket, sizeof(iot_ticket_t)-16);
