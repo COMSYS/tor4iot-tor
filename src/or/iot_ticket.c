3a4f28623c8a5fc476ffc11a6b564bcdd7a5ebb2
Markus Dahlmanns <markus.dahlmanns@rwth-aachen.de>
2018-05-17 09:27:39 +0200

    Problem with MAC

--- src/or/iot_ticket.c
+++ src/or/iot_ticket.c
@@ -62,7 +62,7 @@ void iot_ticket_send(origin_circuit_t *circ) {
   aes_cipher_free(encrypt);
 
   //Compute MAC
-  crypto_hmac_sha256((char*) &(msg->ticket.mac), (char*) iot_mac_key, 16, (char*) &(msg->ticket), sizeof(iot_ticket_t)-DIGEST256_LEN);
+  crypto_hmac_sha256((char*) msg->ticket.mac, (char*) iot_mac_key, 16, (char*) &(msg->ticket), sizeof(iot_ticket_t)-DIGEST256_LEN);
 
   //Set address and port information of IoT device
   inet_pton(AF_INET6, "::1", &(msg->iot_address.in_addr));
