a429238363b980c9f1708b0147f1495b4d6c4954
Markus Dahlmanns <markus.dahlmanns@rwth-aachen.de>
2018-05-25 15:42:17 +0200

    Use OpenSSL

--- src/or/iot_ticket.c
+++ src/or/iot_ticket.c
@@ -16,6 +16,11 @@
 #include "torlog.h"
 #include "connection.h"
 
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/hmac.h>
+
 const uint8_t iot_key[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 const uint8_t iot_mac_key[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
