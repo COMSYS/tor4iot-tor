deecf48c811a0f1fd77452032139aee4cf8d3413
Markus Dahlmanns <markus.dahlmanns@rwth-aachen.de>
2018-07-03 13:53:50 +0200

    Insert missing functions

--- src/common/tortls.c
+++ src/common/tortls.c
@@ -268,6 +268,25 @@ static int tor_dtls_verify_cookie (SSL *ssl, const unsigned char *cookie, unsign
   return 0;
 }
 
+static inline int cval(char c)
+{
+	if (c>='a') return c-'a'+0x0a;
+	if (c>='A') return c-'A'+0x0a;
+	return c-'0';
+}
+
+/* return value: number of bytes in out, <=0 if error */
+static int hex2bin(char *str, unsigned char *out)
+{
+	int i;
+	for(i = 0; str[i] && str[i+1]; i+=2){
+		if (!isxdigit(str[i])&& !isxdigit(str[i+1]))
+			return -1;
+		out[i/2] = (cval(str[i])<<4) + cval(str[i+1]);
+	}
+	return i/2;
+}
+
 static unsigned int psk_server_cb(SSL * ssl, const char *identity,
 	      unsigned char *psk, unsigned int max_psk_len)
 {
