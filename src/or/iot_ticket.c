ca1490e30796aceaedd6e983782318fcf296f4ca
Markus Dahlmanns <markus.dahlmanns@rwth-aachen.de>
2018-08-01 13:54:38 +0200

    Do it on the notice channel

--- src/or/iot_ticket.c
+++ src/or/iot_ticket.c
@@ -281,7 +281,7 @@ iot_join(or_connection_t *conn, const var_cell_t *cell)
         char tbuf[ISO_TIME_USEC_LEN+1];
         format_iso_time_nospace_usec(tbuf, &circ->timestamp_iot_join_complete);
 
-        log_info(LD_GENERAL, "JOINED:%s", tbuf);
+        log_notice(LD_GENERAL, "JOINED:%s", tbuf);
     }
   } else {
     log_info(LD_GENERAL, "Tried to join circuit, but cookies didnt match. 0x%08x ?", ((uint32_t*)cell->payload)[0]);
