e6bb9e0e2df7f245c296695c292c44b1acaee803
Markus Dahlmanns <markus.dahlmanns@rwth-aachen.de>
2019-01-17 13:48:26 +0100

    Reset contrib folder

--- contrib/dist/tor.sh
+++ contrib/dist/tor.sh
@@ -32,7 +32,7 @@ elif [ -f /etc/init.d/functions ]; then
    . /etc/init.d/functions
 fi
 
-TORCTL=/home/markus/tor/install/bin/torctl
+TORCTL=/usr/local/bin/torctl
 
 # torctl will use these environment variables
 TORUSER=_tor
